b88892e14f9f403095a291ac3d61ff71c7663abc
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-21 23:37:05 -0400

    Zustand - todoItem - Todo Completed

--- src/componates/todoItem/index.jsx
+++ src/componates/todoItem/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import useTodoStore from "../../store/todoStore";
 import { motion } from "framer-motion";
 
-const TodoItem = () => {
+const TodoItem = (todo) => {
   const { removeTodo, toggleTodo } = useTodoStore();
   const variants = {
     initial: { opacity: 0, y: 50 },
@@ -14,7 +14,18 @@ const TodoItem = () => {
       initial="initial"
       animate="animate"
       className={`flex items-center p-2`}
-    ></motion.li>
+      Layout
+    >
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+        className="form-checkbox h-5 w-5"
+      />
+      <span className={`flex-1 ml-2 ${todo.completed ? "line-through" : ""}`}>
+        {todo.text}
+      </span>
+    </motion.li>
   );
 };
 
