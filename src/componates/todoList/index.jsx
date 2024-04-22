2119748e2bc2727c282a8efce013157561859eb7
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-21 23:48:54 -0400

    Zustand - todoList - Component Setup

---
+++ src/componates/todoList/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import useTodoStore from "../../store/todoStore";
+import TodoItem from "../todoItem";
+import { AnimatePresence } from "framer-motion";
+
+const TodoList = () => {
+  const todos = useTodoStore((state) => state.todos);
+  return (
+    <AnimatePresence>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </AnimatePresence>
+  );
+};
+
+export default TodoList;
