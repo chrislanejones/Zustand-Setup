f2bdb0ca7151738a1ea0622f6ce48d168e17e6ba
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-23 03:36:15 -0400

    Zustand Todo App - Added Background and Border with Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import TodoList from "./componates/TodoList";
 function App() {
   return (
     <>
-      <main className="dark:bg-slate-700">
+      <main className="bg-gradient-to-r from-slate-900 to-slate-700">
         <div className="container mx-auto max-w-md h-screen py-6">
-          <div className="flex flex-col p-4 border rounded-lg shadow-lg h-full dark:bg-slate-800">
-            <h1 className="text-2xl font-bold mb-4 text-white">Todo List</h1>
-            <AddTodoForm />
-            <TodoList />
+          <div className="h-full rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 p-2">
+            <div className="flex flex-col p-4 rounded-lg shadow-lg h-full dark:bg-slate-800 ">
+              <h1 className="text-2xl font-bold mb-4 text-white">Todo List</h1>
+              <AddTodoForm />
+              <TodoList />
+            </div>
           </div>
         </div>
       </main>
